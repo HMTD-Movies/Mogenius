1ca85ed0d1ae2fb8cb38e93d8d0d58eb3226f7a2
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2022-12-08 13:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://HMTD-Movies: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@github.com/HMTD-Movies/Mogenius $REPO ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://HMTD-Movies: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@github.com/HMTD-Movies/UK-Movies-Bot $REPO okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
