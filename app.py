ac2772135c2fb6d6694924e6bfa5c3fc23bfe902
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2022-12-26 20:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://HMTD-Movies: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@github.com/HMTD-Movies/Force-Subscribers-Bot $REPO okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 fsubbot.py &")
+os.system("git clone https://HMTD-Movies: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@github.com/HMTD-Movies/UK-Movies-Bot $REPO okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
